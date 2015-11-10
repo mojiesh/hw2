4b21da31eccc51f2d1f0f278e7f075a5e087a45f
mojiesh <esh.moji@gmail.com>
2015-11-10 23:12:43 +0100

    that should be enough ;)

--- logmap.cxx
+++ logmap.cxx
@@ -6,14 +6,14 @@ using namespace std;
 int main(){
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
-	int n = 4000;
+	int n = 200;
 	double x[n];
 	ofstream logmap;
 	logmap.open ("logmap.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
-	   x[1] = 0.5;
-	   for(int i = 1; i <= Nskip; i++)
+	   x[0] = 0.5;
+	   for(int i = 0; i <= Nskip; i++)
 		   x[i+1] = r * x[i] * (1 - x[i]);
 	   for(int i = Nskip + 1; i <= Nend; i++){
 	   		   x[i+1] = r * x[i] * (1 - x[i]);
